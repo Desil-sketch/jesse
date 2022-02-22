6b8cd02354bc97e7d81df1100ae794e2a7000bd4
Desil-sketch <60550204+Desil-sketch@users.noreply.github.com>
2022-02-22 14:41:23 -0500

    Add files via upload

--- jesse/routes/__init__.pyx
+++ jesse/routes/__init__.pyx
@@ -3,7 +3,7 @@ from typing import List, Any
 
 import jesse.helpers as jh
 from jesse import exceptions
-from jesse.enums import timeframes
+from jesse.services import logger
 from jesse.models import Route
 
 
@@ -41,8 +41,8 @@ class RouterClass:
         [{'exchange': 'Binance', 'symbol': 'BTC-USD', 'timeframe': '3m'}]
         """
         return [{
-                'exchange': r.exchange, 'symbol': r.symbol, 'timeframe': r.timeframe
-            } for r in self.routes]
+                'exchange': r['exchange'], 'symbol': r['symbol'], 'timeframe': r['timeframe']
+            } for r in self.extra_candles]
 
     def initiate(self, routes: list, extra_routes: list = None):
         if extra_routes is None:
@@ -58,18 +58,21 @@ class RouterClass:
         self.routes = []
 
         for r in routes:
-            # validate strategy
-            strategy_name = r["strategy"]
-            if "pytest" in sys.modules:
-                path = sys.path[0]
-                strategies_dir = f'{sys.path[0]}/jesse/strategies'
-                exists = jh.file_exists(f"{strategies_dir}/{strategy_name}/__init__.py")
+            # validate strategy that the strategy file exists (if sent as a string)
+            if isinstance(r["strategy"], str):
+                strategy_name = r["strategy"]
+                if jh.is_unit_testing():
+                    path = sys.path[0]
+                    strategies_dir = f'{sys.path[0]}/jesse/strategies'
+                    exists = jh.file_exists(f"{strategies_dir}/{strategy_name}/__init__.py")
+                else:
+                    exists = jh.file_exists(f'strategies/{strategy_name}/__init__.py')
             else:
-                exists = jh.file_exists(f'strategies/{strategy_name}/__init__.py')#cpython-39-x86_64-linux-gnu.so'))
-            if not exists:
-                raise exceptions.InvalidRoutes(
-                    f'A strategy with the name of "{strategy_name}" could not be found.')
+                exists = True
 
+            if not exists and isinstance(r["strategy"], str):
+                raise exceptions.InvalidRoutes(
+                    f'A strategy with the name of "{r["strategy"]}" could not be found.')
             self.routes.append(Route(r["exchange"], r["symbol"], r["timeframe"], r["strategy"], None))
 
     def set_market_data(self, routes: List[Any]) -> None:
